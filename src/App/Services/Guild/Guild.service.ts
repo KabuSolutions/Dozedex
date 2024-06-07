38c04389af59d9a89fdc19ec4e60e985a5604706
Deric <dericprado@gmail.com>
2024-06-07 10:53:42 -0300

    Adjusting GuildService

--- src/App/Services/Guild/Guild.service.ts
+++ src/App/Services/Guild/Guild.service.ts
@@ -1,22 +1,12 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment";
-import { Observable, map } from "rxjs";
-import { IGuild } from "../../Interfaces/Guild/IGuild";
+import { IGuild } from "../../Interfaces/Components/Guild/IGuild";
+import { BaseService } from "../Base/BaseService";
+import { IGuildService } from "../../Interfaces/Services/Guild/IGuildService";
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class GuildService {
-
-    private Url: string = `${environment.api}/Guild`;
-    private Jwt: string = environment.jwt_token;
-
-    public constructor(private httpClient: HttpClient) {
-    }
-
-    GetAll(): Observable<IGuild[]> {
-        return this.httpClient.get< {guilds: IGuild[]} >(`${this.Url}/GetAll`).pipe(map(response => response.guilds));
-    }
+export class GuildService extends BaseService<IGuild> implements IGuildService{
+    public override EndPoint: string = "Guild";
 }
