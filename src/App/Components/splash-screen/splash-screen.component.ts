178e821daef7eeb7e2c48d49821da3f12770e0eb
DericPrado <dericprado@gmail.com>
2024-09-26 15:33:35 -0300

    Added altomatic image to gif exchange

--- src/App/Components/splash-screen/splash-screen.component.ts
+++ src/App/Components/splash-screen/splash-screen.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { enviroment } from '../../../Enviroments/enviroment';
 
 @Component({
-  selector: 'app-splash-screen',
+  selector: 'splash-screen',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './splash-screen.component.html',
@@ -11,8 +12,16 @@ import { Component } from '@angular/core';
 export class SplashScreenComponent {
   showSplash: boolean = true;
   showPulsant: boolean = true;
+  splashIcon: string = enviroment.SplashIcon;
+  extension: string = this.splashIcon.substring(this.splashIcon.length - 3);
 
   ngOnInit(): void {
+    if(this.extension != "gif") {
+      this.showPulsant = true;
+    }
+    else {
+      this.showPulsant = false;
+    }
 
     setTimeout(() => {
       this.showSplash = !this.showSplash;
---
+++ src/Enviroments/enviroment.ts
@@ -0,0 +1,3 @@
+export const enviroment = {
+    SplashIcon : "Splash_Screen.gif" // Insira aqui o Gif ou Imagem para a SplashScreen
+}
