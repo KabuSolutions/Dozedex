42d86878bf787fd7c4accaa5264036171580a2c5
Deric <dericprado@gmail.com>
2024-10-12 12:25:17 -0300

    Adjusting SplashScreen component

--- src/App/Components/splash-screen/splash-screen.component.ts
+++ src/App/Components/splash-screen/splash-screen.component.ts
@@ -11,22 +11,24 @@ import { SplashScreenServiceService } from '../../Services/splash-screen-service
   styleUrl: './splash-screen.component.css'
 })
 export class SplashScreenComponent {
-  showSplash: boolean = true;
-  showPulsant: boolean = true;
-  splashIcon: string = enviroment.SplashIcon;
-  extension: string = this.splashIcon.substring(this.splashIcon.length - 3);
-  splashScreenService: SplashScreenServiceService = inject(SplashScreenServiceService);
+  public showSplash: boolean = true;
+  public showPulsant: boolean = true;
+  public splashIcon: string = enviroment.SplashIcon;
+  public extension: string = this.splashIcon.substring(this.splashIcon.length - 3);
+  public splashScreenService: SplashScreenServiceService = inject(SplashScreenServiceService);
 
-  async ngOnInit(): Promise<void> {
+  public constructor() {
+    this.splashScreenService.GetTimeOut().then(SHowSplash => {
+      this.showSplash = SHowSplash
+    });
+  }
+
+  ngOnInit(): void {
     if (this.extension != "gif") {
       this.showPulsant = true;
     }
     else {
       this.showPulsant = false;
     }
-
-    await this.splashScreenService.GetTimeOut().then(SHowSplash => {
-      this.showSplash = SHowSplash
-    });
   }
 }
